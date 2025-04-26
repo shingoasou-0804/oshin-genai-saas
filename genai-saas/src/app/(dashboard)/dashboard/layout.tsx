fe8d19308fcf597ad138067b8dd7b4faeeb02f19
shingoasou-0804 <122670383+shingoasou-0804@users.noreply.github.com>
2025-04-26 11:47:42 +0900

    modify dashboard layout

--- genai-saas/src/app/(dashboard)/dashboard/layout.tsx
+++ genai-saas/src/app/(dashboard)/dashboard/layout.tsx
@@ -18,14 +18,14 @@ export default function DashboardRootLayout({
       </header>
 
       {/* dashboard */}
-      <div>
+      <div className="container md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
         {/* sidebar */}
-        <aside>
-            <div></div>
+        <aside className="fixed md:sticky top-16 z-30 hidden md:block border-r h-[calc(100vh-4.1rem)]">
+            <div className="py-6 px-2 lg:py-8">Dashboard Navigation</div>
         </aside>
 
         {/* main contents */}
-        <main>{children}</main>
+        <main className="flex w-full flex-col overflow-hidden p-4">{children}</main>
       </div>
     </div>
   );
