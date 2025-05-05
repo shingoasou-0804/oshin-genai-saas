a9dc7372508cc1f5cabfe33c76188948cc764d73
shingoasou-0804 <122670383+shingoasou-0804@users.noreply.github.com>
2025-05-05 11:00:14 +0900

    add stable diffusion api key and install axios, form-data

--- genai-saas/src/app/api/generate-image/route.ts
+++ genai-saas/src/app/api/generate-image/route.ts
@@ -1,8 +1,32 @@
 import { NextResponse } from "next/server";
+import axios from "axios";
+import FormData from "form-data";
 
-export default function POST(req: Request) {
+export default async function POST(req: Request) {
     try {
+        const payload = {
+            prompt: "Lighthouse on a cliff overlooking the ocean",
+            output_format: "png"
+        };
+        
+        const response = await axios.postForm(
+            `https://api.stability.ai/v2beta/stable-image/generate/core`,
+            axios.toFormData(payload, new FormData()),
+            {
+                validateStatus: undefined,
+                responseType: "arraybuffer",
+                headers: { 
+                    Authorization: `Bearer ${process.env.STABILITY_API_KEY}`, 
+                    Accept: "image/*" 
+                },
+            },
+        );
 
+        if (response.status !== 200) {
+            throw new Error(`API error: ${response.status}`);
+        }
+
+        console.log(response.data);
     } catch (error) {
         console.error("Error generate image:", error);
         return NextResponse.json(
