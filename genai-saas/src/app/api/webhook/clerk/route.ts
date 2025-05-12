677211e8a533b50c3027912dbce9c6db9ccfe928
shingoasou-0804 <122670383+shingoasou-0804@users.noreply.github.com>
2025-05-12 11:50:21 +0900

    add update and delete user

--- genai-saas/src/app/api/webhook/clerk/route.ts
+++ genai-saas/src/app/api/webhook/clerk/route.ts
@@ -1,8 +1,6 @@
 import { Webhook } from "svix";
-import { headers } from "next/headers";
-import { WebhookEvent } from "@clerk/nextjs/server";
 import { verifyWebhook } from '@clerk/nextjs/webhooks'
-import { createUser } from "@/lib/users";
+import { createUser, deleteUser, updateUser } from "@/lib/users";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
@@ -17,6 +15,34 @@ export async function POST(req: Request) {
         const user = await createUser(id, email);
   
         return NextResponse.json({ user }, { status: 201 });
+      } catch (error) {
+        return NextResponse.json({ error }, { status: 500 });
+      }
+    }
+
+    if (evt.type === "user.updated") {
+      const { id, email_addresses } = evt.data;
+      const email = email_addresses[0].email_address;
+  
+      try {
+        const user = await updateUser(id, email);
+  
+        return NextResponse.json({ user }, { status: 200 });
+      } catch (error) {
+        return NextResponse.json({ error }, { status: 500 });
+      }
+    }
+
+    if (evt.type === "user.deleted") {
+      const { id } = evt.data;
+      if (!id) {
+        throw new Error("Failed to Delete User Table");
+      }
+  
+      try {
+        const user = await deleteUser(id);
+  
+        return NextResponse.json({ user }, { status: 200 });
       } catch (error) {
         return NextResponse.json({ error }, { status: 500 });
       }
--- genai-saas/src/lib/users.ts
+++ genai-saas/src/lib/users.ts
@@ -18,3 +18,32 @@ export async function createUser(clerkId: string, email: string) {
     return NextResponse.json({ error }, { status: 500 });
   }
 }
+
+export async function updateUser(clerkId: string, email: string) {
+  try {
+    const user = await prisma.user.update({
+      where: { clerkId: clerkId },
+      data: {
+        email: email,
+      },
+    });
+
+    return NextResponse.json({ user }, { status: 200 });
+  } catch (error) {
+    console.error("Failed to create User:", error);
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
+
+export async function deleteUser(clerkId: string) {
+  try {
+    const user = await prisma.user.delete({
+      where: { clerkId: clerkId },
+    });
+
+    return NextResponse.json({ user }, { status: 200 });
+  } catch (error) {
+    console.error("Failed to create User:", error);
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
