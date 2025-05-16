fa4a329753bddc724e382a65df6e85ed69123782
shingoasou-0804 <122670383+shingoasou-0804@users.noreply.github.com>
2025-05-17 06:24:31 +0900

    add stylying to plan page

--- genai-saas/src/app/(dashboard)/dashboard/plan/page.tsx
+++ genai-saas/src/app/(dashboard)/dashboard/plan/page.tsx
@@ -11,36 +11,118 @@ const Plan = () => {
         </p>
       </div>
 
-      <div>
+      <div className="grid lg:grid-cols-3 gap-3 md:grid-cols-1 mx-auto max-w-7xl">
         <div className="border rounded-xl bg-card p-8 shadown-sm flex flex-col">
-          <div>
-            <div>
-              <div>
-                <SparkleIcon />
-                <h2>Starter</h2>
+          <div className="space-y-6">
+            <div className="space-y-4">
+              <div className="flex items-center gap-2">
+                <SparkleIcon className="size-6 text-primary" />
+                <h2 className="text-2xl font-bold">Starter</h2>
               </div>
-              <p>個人利用に最適なエントリープラン</p>
+              <p className="text-muted-foreground">個人利用に最適なエントリープラン</p>
             </div>
 
-            <div>
-              <span>1000</span>
-              <span>/月</span>
+            <div className="flex items-baseline">
+              <span className="text-4xl font-bold">1000</span>
+              <span className="ml-2 text-muted-foreground">/月</span>
             </div>
 
-            <ul>
-              <li>
-                <Check />
+            <ul className="space-y-4 text-sm">
+              <li className="flex items-center gap-3">
+                <Check className="size-4 text-primary" />
                 <span>月50クレジット付与</span>
               </li>
-              <li>
-                <Check />
+              <li className="flex items-center gap-3">
+                <Check className="size-4 text-primary"/>
                 <span>基本的な画像生成</span>
               </li>
             </ul>
           </div>
 
           <form>
-            <Button>Starterプランを選択</Button>
+            <Button
+              className="w-full mt-8"
+              size={"lg"}
+              variant={"outline"}
+              type="submit"
+            >
+              Starterプランを選択</Button>
+          </form>
+        </div>
+
+        <div className="border rounded-xl bg-card p-8 shadown-sm flex flex-col">
+          <div className="space-y-6">
+            <div className="space-y-4">
+              <div className="flex items-center gap-2">
+                <SparkleIcon className="size-6 text-primary" />
+                <h2 className="text-2xl font-bold">Starter</h2>
+              </div>
+              <p className="text-muted-foreground">個人利用に最適なエントリープラン</p>
+            </div>
+
+            <div className="flex items-baseline">
+              <span className="text-4xl font-bold">1000</span>
+              <span className="ml-2 text-muted-foreground">/月</span>
+            </div>
+
+            <ul className="space-y-4 text-sm">
+              <li className="flex items-center gap-3">
+                <Check className="size-4 text-primary" />
+                <span>月50クレジット付与</span>
+              </li>
+              <li className="flex items-center gap-3">
+                <Check className="size-4 text-primary"/>
+                <span>基本的な画像生成</span>
+              </li>
+            </ul>
+          </div>
+
+          <form>
+            <Button
+              className="w-full mt-8"
+              size={"lg"}
+              variant={"outline"}
+              type="submit"
+            >
+              Starterプランを選択</Button>
+          </form>
+        </div>
+
+        <div className="border rounded-xl bg-card p-8 shadown-sm flex flex-col">
+          <div className="space-y-6">
+            <div className="space-y-4">
+              <div className="flex items-center gap-2">
+                <SparkleIcon className="size-6 text-primary" />
+                <h2 className="text-2xl font-bold">Starter</h2>
+              </div>
+              <p className="text-muted-foreground">個人利用に最適なエントリープラン</p>
+            </div>
+
+            <div className="flex items-baseline">
+              <span className="text-4xl font-bold">1000</span>
+              <span className="ml-2 text-muted-foreground">/月</span>
+            </div>
+
+            <ul className="space-y-4 text-sm">
+              <li className="flex items-center gap-3">
+                <Check className="size-4 text-primary" />
+                <span>月50クレジット付与</span>
+              </li>
+              <li className="flex items-center gap-3">
+                <Check className="size-4 text-primary"/>
+                <span>基本的な画像生成</span>
+              </li>
+            </ul>
+          </div>
+
+          <form>
+            <Button
+              className="w-full mt-8"
+              size={"lg"}
+              variant={"outline"}
+              type="submit"
+            >
+              Starterプランを選択</Button>
           </form>
         </div>
       </div>
